9f3405b8fa7b69738a9bdea457955c2e74156dae
Wan Li <wl9wgc@virginia.edu>
2020-05-26 14:07:43 -0400

    Updates

---
+++ precache-manifest.81546e798c2277a074729c08b8a991cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee54ca12b3fcf35e2643ce2ef8384ef",
+    "url": "/War/index.html"
+  },
+  {
+    "revision": "6d2abed5906bb76ad4a0",
+    "url": "/War/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ed8eb1db9cb3ce93e23",
+    "url": "/War/static/js/2.939ccd3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/War/static/js/2.939ccd3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d2abed5906bb76ad4a0",
+    "url": "/War/static/js/main.fc802e6f.chunk.js"
+  },
+  {
+    "revision": "b546a73f2f39ef2a078e",
+    "url": "/War/static/js/runtime-main.15aa53f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/War/static/media/logo.5d5d9eef.svg"
+  }
+]);
